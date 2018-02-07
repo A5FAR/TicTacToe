8349f4ba03c86d01676091a1841c04097aba724b
ASFAR <asfar_666@live.com>
2018-02-07 09:28:52 +0600

    classWork

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,14 @@
-//JS code goes here
+
+var turn = false;
+var lastID = null;
+function play(event) {
+
+
+	if(!event.target.innerText){
+		event.target.innerText = turn?"O": "X";
+		turn = !turn;
+		lastID = id;
+	}
+
+	
+}
