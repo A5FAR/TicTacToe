ac7154dc04aef57a112b5f41d25d96cc17ea9b46
ASFAR <asfar_666@live.com>
2018-02-15 20:54:39 +0600

    fixed misplacement of bracket

--- tictactoe.js
+++ tictactoe.js
@@ -17,35 +17,37 @@ function play(event , id) {
 		turn = !turn;
 		lastID = event.target.innerText;
 		x++;
-	}
+	
 
-	if(x >= 5){
-		for (var i = 0; i < 9; i++) {
-				block[i] = document.getElementById(i).innerText;
-			}
+		if(x >= 5){
+			for (var i = 0; i < 9; i++) {
+					block[i] = document.getElementById(i).innerText;
+				}
 
-			if ((block[0]==block[1] && block[1]==block[2] && block[0]!="") || (block[3]==block[4] && block[4]==block[5] && block[3]!="") || (block[6]==block[7] && block[7]==block[8] && block[6]!="") || (block[0]==block[3] && block[3]==block[6] && block[0]!="") || (block[1]==block[4] && block[4]==block[7] && block[1]!="") || (block[2]==block[5] && block[5]==block[8] && block[2]!="") || (block[0]==block[4] && block[4]==block[8] && block[0]!="") || (block[2]==block[4] && block[4]==block[6] && block[2]!="")) {
-				winner = lastID;
-			}	
-	}
+				if ((block[0]==block[1] && block[1]==block[2] && block[0]!="") || (block[3]==block[4] && block[4]==block[5] && block[3]!="") || (block[6]==block[7] && block[7]==block[8] && block[6]!="") || (block[0]==block[3] && block[3]==block[6] && block[0]!="") || (block[1]==block[4] && block[4]==block[7] && block[1]!="") || (block[2]==block[5] && block[5]==block[8] && block[2]!="") || (block[0]==block[4] && block[4]==block[8] && block[0]!="") || (block[2]==block[4] && block[4]==block[6] && block[2]!="")) {
+					winner = lastID;
+				}	
+		}
 
-	if(winner == "X"){
-		document.getElementById("status").innerText="Player 1 Wins";
-		p1_w++;
-		p2_l++;
-		document.getElementById("wCount1").innerText=p1_w;
-		document.getElementById("lCount1").innerText=p2_l;
-	}
-	else if (winner == "O"){
-		document.getElementById("status").innerText="Player 2 Wins";
-		p1_l++;
-		p2_w++;
-		document.getElementById("wCount2").innerText=p2_w;
-		document.getElementById("lCount2").innerText=p1_l;
-	}
+		if(winner == "X"){
+			document.getElementById("status").innerText="Player 1 Wins";
+			p1_w++;
+			p2_l++;
+			document.getElementById("wCount1").innerText=p1_w;
+			document.getElementById("lCount1").innerText=p2_l;
+		}
+		else if (winner == "O"){
+			document.getElementById("status").innerText="Player 2 Wins";
+			p1_l++;
+			p2_w++;
+			document.getElementById("wCount2").innerText=p2_w;
+			document.getElementById("lCount2").innerText=p1_l;
+		}
 
-	if(x == 9){
-		document.getElementById("status").innerText="Draw Game";
+		if(x == 9){
+			document.getElementById("status").innerText="Draw Game";
+		}
+
 	}
 }
 
